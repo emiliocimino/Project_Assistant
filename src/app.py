cbf4a1e4606f350833b6da59e5b0a046b22a2367
emiliocimino <emilio.cimino@outlook.it>
2026-08-29 21:12:08 +0200

    fix

--- src/app.py
+++ src/app.py
@@ -394,6 +394,7 @@ with gr.Blocks(title="Wiki Agent") as ui:
 if __name__ == "__main__":
     if not os.path.isdir(DATA_DIR):
         os.mkdir(DATA_DIR)
+    if not os.path.isdir(DATA_DIR / "wiki"):
         os.mkdir(DATA_DIR / "wiki")
         os.mkdir(DATA_DIR / "sources")
     ui.launch(inbrowser=True, **LAUNCH_STYLE, server_port=7860)
